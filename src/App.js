c0a56c437e10026787a2aaa813265a57a1228b6e
Jasur <ibraimov0409@mail.ru>
2020-09-04 04:59:25 +0600

    header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import Header from './components/header'
+import './app.scss'
 function App() {
 	return (
 		<div>
-			<h1>Hello world</h1>
+			<Header />
 		</div>
 	)
 }
---
+++ src/components/header/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './styles.scss'
+const Header = () => (
+	<header>
+		<div className='wrapper'>
+			<div className='logo'>
+				<img
+					src='https://image.flaticon.com/icons/svg/888/888879.svg'
+					alt='logo'
+				/>
+				Logo
+			</div>
+		</div>
+	</header>
+)
+
+export default Header
